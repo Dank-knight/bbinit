989ec978bede89ece5716a639960ae0b6de2c033
Dank-knight <dshtykh@gmail.com>
2022-03-19 18:57:44 +0200

    init commit

---
+++ src/main/java/com/dankknightkh/bbinit/BbinitApplication.java
@@ -0,0 +1,13 @@
+package com.dankknightkh.bbinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dankknightkh/bbinit/BbinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dankknightkh.bbinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

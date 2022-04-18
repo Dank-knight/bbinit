bd8cf227c4800198677dc9edabdd5bc32fafe0ed
Dank-knight <dshtykh@gmail.com>
2022-04-18 21:07:22 +0300

    BbinitApplicationTests  - init commit with test setup

--- src/test/java/com/dankknightkh/bbinit/BbinitApplicationTests.java
+++ src/test/java/com/dankknightkh/bbinit/BbinitApplicationTests.java
@@ -1,13 +1,28 @@
 package com.dankknightkh.bbinit;
 
+import com.dankknightkh.bbinit.command.catalog.CommandCatalog;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest
 class BbinitApplicationTests {
 
+	@Mock
+	private CommandCatalog commandCatalog;
+
+	@InjectMocks
+	private BbInitApplication bbInitApplication;
+
+
 	@Test
 	void contextLoads() {
+		assertThat(bbInitApplication, is(notNullValue()));
 	}
 
 }

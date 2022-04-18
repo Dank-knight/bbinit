ba3bdcaef02af6838e1a0ac94d6cb9267261b999
Dank-knight <dshtykh@gmail.com>
2022-04-18 22:26:34 +0300

    BbinitApplicationTests  - add spring boot test with two params

--- src/test/java/com/dankknightkh/bbinit/BbinitApplicationTests.java
+++ src/test/java/com/dankknightkh/bbinit/BbinitApplicationTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(args={"-p=training", "-c=noCommand"})
 class BbinitApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertThat(true, org.hamcrest.Matchers.is(true));
+		assertThat(true, is(true));
 	}
 
 }
